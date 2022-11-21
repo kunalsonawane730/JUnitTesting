dfcb2b3d02f57589371bc474f03afd1202aed61a
Kunal <kunalsonawane730@gmail.com>
2022-11-21 18:05:27 +0530

    adding welcome message for JUnit Testing

---
+++ src/main/java/com/bridgelabz/day6_junittesting/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.day6_junittesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome TO JUnit Testing Programs" );
+    }
+}
